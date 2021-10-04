3788b83a7965397af8c968970854664878eb55bf
lhankel <leland.hankel@gmail.com>
2021-10-04 14:34:51 -0400

    class 10/4

--- src/comp/comp152/Doctor.java
+++ src/comp/comp152/Doctor.java
@@ -11,7 +11,7 @@ public class Doctor {
     }
 
     public double billPatient(Person patient){
-
+        patient.payBill(fee);
         return fee;
     }
 
--- src/comp/comp152/GovernmentProgram.java
+++ src/comp/comp152/GovernmentProgram.java
@@ -1,4 +1,23 @@
 package comp.comp152;
 
+import java.util.Random;
+
 public class GovernmentProgram {
+    private String programType;
+
+
+    public GovernmentProgram(){
+        String[] types ={"Medicare", "Medicaid", "VA", "ObamaCare", "Insuricare"};
+        var numberPicker = new Random();
+        var pick = numberPicker.nextInt(5);
+        programType = types[pick];
+    }
+
+    public void payBill(double bill){
+        if (programType.equals("Insuricare"))
+            System.out.println(programType + "Just paid a bill of " + bill+3);
+        else
+            System.out.println(programType+" Just paid a bill of "+ bill);
+    }
+
 }
