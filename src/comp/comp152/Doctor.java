7dc950b73439e4b3854a0558ff767d1288e74679
lhankel <leland.hankel@gmail.com>
2021-10-04 14:58:15 -0400

    class 10/4

--- src/comp/comp152/Doctor.java
+++ src/comp/comp152/Doctor.java
@@ -4,6 +4,12 @@ public class Doctor {
     protected String AlmaMater;
     public double fee;
 
+    public Doctor(String MedSchool, double fee){
+         AlmaMater = MedSchool;
+         this.fee = fee;
+
+    }
+
     public void treatPatient(Person patient){
         System.out.println("Basic place holder");
 
