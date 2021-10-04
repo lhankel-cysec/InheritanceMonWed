593588ad72e9561d3fcac490c754356d08d13535
lhankel <leland.hankel@gmail.com>
2021-10-04 14:17:05 -0400

    class 10/4

--- src/comp/comp152/Person.java
+++ src/comp/comp152/Person.java
@@ -1,4 +1,16 @@
 package comp.comp152;
 
 public class Person {
+    private String Name;
+    public Person(String Name){
+        this.Name = Name;
+    }
+
+    public void getTreated(){
+        System.out.println("My name is " + Name + "and I feel so much better!");
+    }
+
+    public void payBill(double bill){
+        System.out.println("Ouch, I need to pay a bill off "+ bill);
+    }
 }
